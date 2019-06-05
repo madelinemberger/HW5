90eb7254073ed641d865298727deb38ece2e3236
Madeline Berger <maddiemt8@gmail.com>
2019-06-05 13:04:18 -0700

    added data to package

--- R/hello.R
+++ R/hello.R
@@ -1,6 +1,6 @@
 # Hello, world!
 #
-# This is an example function named 'hello' 
+# This is an example function named 'hello'
 # which prints 'Hello, world!'.
 #
 # You can learn more about package authoring with RStudio at:
@@ -13,6 +13,17 @@
 #   Check Package:             'Cmd + Shift + E'
 #   Test Package:              'Cmd + Shift + T'
 
+
+library(tidyverse)
+library(roxygen2)
+library(devtools)
+
 hello <- function() {
   print("Hello, world!")
 }
+
+temp <- read_csv("temp (1).csv")
+precip <- read_csv("precipitation (1).csv")
+
+use_data(temp, pkg=HW5)
+use_data(precip, pkg=HW5)
